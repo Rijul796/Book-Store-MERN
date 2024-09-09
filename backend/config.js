d132c6a59482a1bd8ec608e63a8d2105e7cbe0b7
rijul <rijulsharma.1972@gmail.com>
2024-09-09 15:41:39 +0530

    Final

---
+++ backend/config.js
@@ -0,0 +1,5 @@
+import "dotenv/config";
+
+export const PORT = 5555;
+
+export const mongoDBURL='mongodb+srv://erenyeager101003:eren123@books-store-mern.6fnt2.mongodb.net/?retryWrites=true&w=majority&appName=Books-Store-MERN';
